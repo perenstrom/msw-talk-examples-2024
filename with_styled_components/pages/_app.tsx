9269ac5ef6cfd6c27ec3c480c70750f479002ba7
Per Enström <enstrom.per@gmail.com>
2024-04-09 07:48:44 +0200

    Pick mui flavour

--- with_styled_components/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-
-function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  return <Component {...pageProps} key={router.asPath} />;
-}
-
-export default MyApp;
